c7d7c459dab0bdcd16910a821217bec43ce6ad2d
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2020-10-30 06:27:53 +0200

    normalize the space for xpath

--- travelData/spiders/booking.py
+++ travelData/spiders/booking.py
@@ -32,49 +32,34 @@ class BookingSpider(scrapy.Spider):
 
             yield {
                 "source": 'Expedia',
-                "location": location,
-                'img': img,
                 'name': name,
                 "rating": str(rating) + str(total_rating),
                 "price": price,
+                "location": location,
             }
-        # a = resp.xpath("//div/div[@class='sr_item_content sr_item_content_slider_wrapper ']/div[@class='sr_rooms_table_block clearfix sr_card_rooms_container']/div[@class='room_details ']/div/div/div/div[2]/div/div[2]/div/div")
-        # print(a)
-        # prices = resp.xpath()
-        # hotels = response.xpath("//div[@id='hotellist_inner']/div[@class='sr_item  sr_item_new sr_item_default sr_property_block sr_card_no_hover  sr_flex_layout          ']")
-        # hotels = response.xpath("//div[@id='hotellist_inner']//img")
-        # yield {
-        #     'url' : response.url ,
-        # }
+
         hotels = response.xpath("//div[@id='hotellist_inner']/div")
-        # //div[@id='hotellist_inner']/div/div[@class='sr_item_content sr_item_content_slider_wrapper ']/div[@class='sr_rooms_table_block clearfix sr_card_rooms_container']/div/div/div/div/div[@class='roomPrice roomPrice_flex  sr_discount ']/div/div[2]/div/div/text()
-        # print(hotels)
-# //div[@id='hotellist_inner']/div/div[@class='sr_item_content sr_item_content_slider_wrapper ']/div[@class='sr_rooms_table_block clearfix sr_card_rooms_container']/div[@class='room_details ']/div/div/div/div[2]/div/div[2]/div/span
+
         for hotel in hotels:
             # print(hotel)
             img = hotel.xpath(".//img/@src").get()
-            title = hotel.xpath(".//h3/a/span/text()").get()
-            rating = hotel.xpath(".//div[@class='bui-review-score__badge']/text()").get()
-            link = hotel.xpath(".//h3/a/@href").get()
-            room = hotel.xpath(".//div[@class='sr_item_content sr_item_content_slider_wrapper ']/div[@class='sr_rooms_table_block clearfix sr_card_rooms_container']/div/div/div/div/div[@class='roomPrice roomPrice_flex  sr_discount ']/div/div[1]/div[@class='bui-price-display__label prco-inline-block-maker-helper']/text()").get()
+            title = hotel.xpath("normalize-space(.//h3/a/span/text())").get()
+            rating = hotel.xpath("normalize-space(.//div[@class='bui-review-score__badge']/text())").get()
+            link = hotel.xpath("normalize-space(.//h3/a/@href)").get()
+            room = hotel.xpath("normalize-space(.//div[@class='sr_item_content sr_item_content_slider_wrapper ']/div[@class='sr_rooms_table_block clearfix sr_card_rooms_container']/div/div/div/div/div[@class='roomPrice roomPrice_flex  sr_discount ']/div/div[1]/div[@class='bui-price-display__label prco-inline-block-maker-helper']/text())").get()
             yy = hotel.xpath("normalize-space(.//div[@class='sr_item_content sr_item_content_slider_wrapper ']/div[@class='sr_rooms_table_block clearfix sr_card_rooms_container']/div/div/div/div/div[@class='roomPrice roomPrice_flex  sr_discount ']/div/div[2]/div/div/text())").get()
             try:
                 # pass
                 yield response.follow(url=link, callback=self.parse_hotel, meta={'title': title, 'rating': rating, 'img': img, 'y': yy, 'room': room})
             except Exception as e:
-                pass
+                yield {
+                    "source": 'Booking',
+                    "name": title,
+                    "rating": rating,
+                    "price": str(yy.replace('\xa0', ' ')) + ' - ' + str(room),
+                    'location':'N/A',
+                }
 
-
-            # price = hotel.xpath(".//div[@class='prco-inline-block-maker-helper']/div//text()").get()
-            # price2 = hotel.xpath(".//div[@class='sr_item_content sr_item_content_slider_wrapper ']/div[@class='sr_rooms_table_block clearfix sr_card_rooms_container']/div[@class='room_details ']/div/div/div/div[2]/div/div[2]/div/span//text()").get()
-            # print("//////////////////////////")
-            # print(hotel.xpath(".//div[2]/div[3]"))
-            # yield {
-            #     "img": img,
-            #     "title": title,
-            #     "rating": rating,
-            #     "price": price2,
-            # }
 
     def parse_hotel(self, response):
         img = response.request.meta['img']
@@ -83,29 +68,21 @@ class BookingSpider(scrapy.Spider):
         yy = response.request.meta['y']
         room = response.request.meta['room']
 
-        x = response.xpath("//div[@id='property_description_content']/p")
-        z = response.xpath("//p[@id='showMap2']/span/text()").get()
-        pric = response.xpath("//div[@class='hprt-price-block ']/div/div/div[2]/span/text()")
-        d = response.xpath("//div[@class='bui-price-display__value prco-inline-block-maker-helper prco-font16-helper']/text()")
-        # print(len(pric))
-        # for i in pric:
-        #     print('/////////////////')
-        #     print(i)
-        # print('/////////////////')
-        # print(response.xpath("//div[@id='rooms_table']/div[@id='available_rooms']/div[@id='maxotelRoomArea']"))
-        t = ""
-        for i in x:
-            t += i.xpath(".//text()").get()
+
+        z = response.xpath("normalize-space(//p[@id='showMap2']/span/text())").get()
+
+
+
+        # More info
+        # x = response.xpath("//div[@id='property_description_content']/p")
+        # t = ""
+        # for i in x:
+        #     t += i.xpath(".//text()").get()
 
         yield {
             "source": 'Booking',
-            "img": img,
-            "title": title,
+            "name": title,
             "rating": rating,
-            "t": t,
-            # "price": pric.get(),
-            "y": yy.replace('\xa0', ' '),
-            "room": room,
-            'z':z,
-            # "d": d.get()
+            "price": str(yy.replace('\xa0', ' ')) + ' - ' + str(room),
+            'location':z,
         }
